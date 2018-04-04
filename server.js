074a9c6a2d0f785675479d20f7db9fa376767f79
Grzegorz Szermiński <szerman@poczta.fm>
2018-04-04 12:29:10 +0200

    Work in progress

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+
+var app = express();
+
+app.listen(3000);
+
+app.get('/', function (req, res) {
+    console.log('Otrzymałem żądanie GET do strony głównej');
+    res.send('Hello GET!');
+});
+
+app.post('/', function (req, res) {
+    console.log('Otrzymałem żądanie POST do strony głównej');
+    res.send('Hello POST!');
+});
+
+app.delete('/del_user', function (req, res) {
+    console.log('Otrzymałem żądanie DELETE do strony /del_user');
+    res.send('Hello DELETE!');
+});
+
+app.get('/list_user', function (req, res) {
+    console.log('Otrzymałem żądanie GET do strony /list_user');
+    res.send('Strona z listą użytkowników!');
+});
+
+app.get('/ab*cd', function(req, res) {
+    console.log('Otrzymałem żądanie GET do strony /ab*cd');
+    res.send('Wzór pasuje');
+});
